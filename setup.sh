3c8cc6e1f85548aac265229a77654abc517182bc
nik <nik@pop-os.localdomain>
2022-09-12 23:45:15 +0200

    Added Pop!OS settings

--- setup.sh
+++ setup.sh
@@ -13,9 +13,12 @@ python3 -m pip install --user argcomplete
 activate-global-python-argcomplete --user
 source ~/.bashrc
 
-# Install roles
+# Install roles / collections
 #ansible-galaxy install gantsign.visual-studio-code
 ansible-galaxy install -r requirements.yml
+
+# Install python requirements for collections
+sudo apt install -y python3-psutil
 
 # Run ansible!
 ansible-playbook playbook.yaml
